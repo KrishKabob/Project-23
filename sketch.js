98b22adf3ebfa4885fb0a078d1c27645c6e99c28
KrishKabob <63270377+KrishKabob@users.noreply.github.com>
2020-06-04 18:12:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2
+	packageSprite.velocityX = 2;
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale = 0.6;
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.1, isStatic:true});
+	World.add(world, packageBody);
+	box1 = new box(400,640,150,20);
+	box2 = new box(350,600,20,100);
+	box3 = new box(450,600,20,100);
+	
+
+	
+	
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle (width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  box1.display();
+  box2.display();
+  box3.display();
+  if(helicopterSprite.x>800||helicopterSprite.x<0){
+	helicopterSprite.velocityX = helicopterSprite.velocityX*-1;
+}
+if(packageSprite.x>800||packageSprite.x<0){
+	packageSprite.velocityX = packageSprite.velocityX*-1;
+	packageBody.velocityX = packageBody.velocityX*-1;
+}
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyDown("down")) {
+	Matter.Body.setStatic(packageBody,false);
+	packageSprite.velocityX = 0;
+	packageBody.velocityX = 0;
+	}
+	
+}
+
+
+
